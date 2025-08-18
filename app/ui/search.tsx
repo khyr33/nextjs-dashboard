7676780ba9076a267ef651912ba6bdc5781310b9
khyr33 <khyr33@gmail.com>
2025-08-18 03:44:17 -0400

    Chapter 11 done

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,11 +13,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+    replace(`${pathname}?${params.toString()}`);
   }, 300);
 
   return (
